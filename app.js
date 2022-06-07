fc016aec24adbddb851378f69a344dc9b6c79016
Suvashish Shrestha <77734238+suvashish26@users.noreply.github.com>
2022-06-07 22:52:37 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function myPassword(){
+    var x = document.getElementById('myPassword');
+    if(x.type === 'password'){
+        x.type = 'text';
+
+    }else{
+        x.type = 'password';
+    }
+}
